413cf1a207a34848ecf0d70fa89e9edda6810642
nhuthq <>
2023-01-12 16:11:25 +0700

    add new fonts

--- Airbnb/src/screens/Home/index.js
+++ Airbnb/src/screens/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text, ImageBackground, Pressable} from 'react-native';
 import styles from './style';
-import Fontisto from 'react-native-vector-icons/Fontisto';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 function HomeScreen() {
   return (
@@ -9,7 +9,7 @@ function HomeScreen() {
       <Pressable
         style={styles.searchBtn}
         onPress={() => console.log('Search Btn On Press')}>
-        <Fontisto name="search" size={25} color="#f15454" />
+        <FontAwesome name="search" size={25} color="#f15454" />
         <Text style={styles.searchBtnText}>Where are you going</Text>
       </Pressable>
       <ImageBackground
